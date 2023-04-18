ccb3b13d4ebe21531e28da6b75777627dd922f8f
yurika <yurika@localhost>
2023-04-18 10:34:02 +0900

    task4最新

--- src/main/java/jp/co/seattle/library/commonutil/BookUtil.java
+++ src/main/java/jp/co/seattle/library/commonutil/BookUtil.java
@@ -78,7 +78,7 @@ public class BookUtil {
 	private static boolean isValidIsbn(String isbn) {
 		//TODO　ISBNが半角数字で10文字か13文字であればtrue（タスク４）
 		if (!isbn.isEmpty()) {
-			if ((isbn.matches("^[0-9]*$")) && (isbn.length() == 10 || isbn.length() == 13)) {
+			if (isbn.matches("^[0-9]*$") && (isbn.length() == 10 || isbn.length() == 13)) {
 				return true;
 			} else {
 				return false;
