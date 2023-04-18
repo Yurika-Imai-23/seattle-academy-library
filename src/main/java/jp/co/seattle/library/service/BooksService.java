1cdad5b13fb22989dd8017e39b34258cdfc66974
yurika <yurika@localhost>
2023-04-18 11:37:23 +0900

    task4最新

--- src/main/java/jp/co/seattle/library/service/BooksService.java
+++ src/main/java/jp/co/seattle/library/service/BooksService.java
@@ -61,7 +61,6 @@ public class BooksService {
 	 */
 	public int registBook(BookDetailsInfo bookInfo) {
 		// TODO 取得した書籍情報を登録し、その書籍IDを返却するようにSQLを修正（タスク４）
-
 		String sql = "INSERT INTO books (title, author, publisher, publish_date, thumbnail_name, thumbnail_url, isbn, description, reg_date, upd_date) VALUES (?, ?, ?, ?, ?, ?, ?, ?, now(), now()) RETURNING id;";
 
 		int bookId = jdbcTemplate.queryForObject(sql, int.class, bookInfo.getTitle(), bookInfo.getAuthor(),
@@ -90,12 +89,12 @@ public class BooksService {
 		String sql;
 		if (bookInfo.getThumbnailUrl() == null) {
 			// TODO 取得した書籍情報を更新するようにSQLを修正（タスク５）
-			sql = "update books set title = ? , author = ? , publisher = ? , publish_date = ? , isbn = ? , description = ? , upd_date = now() where　id = ?;";
+			sql = "";
 			jdbcTemplate.update(sql, bookInfo.getTitle(), bookInfo.getAuthor(), bookInfo.getPublisher(),
 					bookInfo.getPublishDate(), bookInfo.getIsbn(), bookInfo.getDescription(), bookInfo.getBookId());
 		} else {
 			// TODO 取得した書籍情報を更新するようにSQLを修正（タスク５）
-			sql = "update books set title = ? , author = ? , publisher = ? ,publish_date = ? , thumbnail_name = ? , thumbnail_url = ? , isbn = ? , description = ?, upd_date = now() where id = ?;";
+			sql = "";
 			jdbcTemplate.update(sql, bookInfo.getTitle(), bookInfo.getAuthor(), bookInfo.getPublisher(),
 					bookInfo.getPublishDate(), bookInfo.getThumbnailName(), bookInfo.getThumbnailUrl(),
 					bookInfo.getIsbn(), bookInfo.getDescription(), bookInfo.getBookId());
